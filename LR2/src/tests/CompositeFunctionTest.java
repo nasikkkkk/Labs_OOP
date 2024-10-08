8538550a40b7ec5b0782dabc81fddc0495d2dd32
Kseteny <1kseteni1@gmail.com>
2024-10-08 16:30:00 +0400

    X

---
+++ LR2/src/tests/CompositeFunctionTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import functions.SimpleIterations;
+import functions.IdentityFunction;
+import functions.CompositeFunction;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CompositeFunctionTest {
+
+    @Test
+    void apply() {
+        SimpleIterations fun_1 = new SimpleIterations();
+        IdentityFunction fun_2 = new IdentityFunction();
+        CompositeFunction fun_3 = new CompositeFunction(fun_1, fun_2);
+        Assertions.assertEquals(fun_3.apply(3), fun_3.apply(3));
+        Assertions.assertEquals(fun_3.apply(5), fun_3.apply(5));
+        Assertions.assertEquals(fun_3.apply(6), fun_3.apply(6));
+        Assertions.assertEquals(fun_3.apply(9), fun_3.apply(9));
+    }
+}
