279d24d298bd50abfabd536ef4340f6cc62f562a
Kseteny <1kseteni1@gmail.com>
2024-10-09 14:34:00 +0400

    X

---
+++ LR2/src/tests/hardFunctions.java
@@ -0,0 +1,16 @@
+package tests;
+
+import functions.IdentityFunction;
+import functions.SqrFunction;
+import functions.CompositeFunction;
+import functions.NewtonMethod;
+import functions.SimpleIterations;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class hardFunctions {
+    @Test
+    void test() {
+        
+    }
+}
