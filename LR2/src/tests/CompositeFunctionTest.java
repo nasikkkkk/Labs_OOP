2fdac79f5409235a8dfe337a144cb7bbeccfbc1f
Kseteny <1kseteni1@gmail.com>
2024-10-10 09:14:50 +0400

    tests

--- LR2/src/tests/CompositeFunctionTest.java
+++ LR2/src/tests/CompositeFunctionTest.java
@@ -11,9 +11,9 @@ class CompositeFunctionTest {
         SimpleIterations fun_1 = new SimpleIterations (new SqrFunction());
         IdentityFunction fun_2 = new IdentityFunction();
         CompositeFunction fun_3 = new CompositeFunction(fun_1, fun_2);
-        Assertions.assertEquals(fun_3.apply(3), fun_3.apply(3));
-        Assertions.assertEquals(fun_3.apply(5), fun_3.apply(5));
-        Assertions.assertEquals(fun_3.apply(6), fun_3.apply(6));
-        Assertions.assertEquals(fun_3.apply(9), fun_3.apply(9));
+        Assertions.assertEquals(1, fun_3.andThen(fun_1).andThen(fun_2).apply(3), 0.0001);
+        Assertions.assertEquals(fun_3.apply(5), fun_3.andThen(fun_1).andThen(fun_2).apply(5), 0.0001);
+        Assertions.assertEquals(fun_3.apply(6), fun_3.andThen(fun_1).andThen(fun_2).apply(6), 0.0001);
+        Assertions.assertEquals(fun_3.apply(9), fun_3.andThen(fun_1).andThen(fun_2).apply(9), 0.0001);
     }
 }
