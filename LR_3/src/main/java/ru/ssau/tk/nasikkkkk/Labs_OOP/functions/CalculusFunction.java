64a567ec87f809803f2bb33357f7bb7e5e880078
nasikkkkk <147999508+nasikkkkk@users.noreply.github.com>
2024-10-30 16:14:50 +0400

    Delete

--- LR_3/src/main/java/ru/ssau/tk/nasikkkkk/Labs_OOP/functions/CalculusFunction.java
+++
@@ -1,9 +0,0 @@
-package ru.ssau.tk.nasikkkkk.Labs_OOP.functions;
-
-public interface CalculusFunction extends MathFunction {
-    double calculateDerivative(double x);
-    double calculateIntegral(double x0, double x);
-
-    @Override
-    double apply(double x);
-}
