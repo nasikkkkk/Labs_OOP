c05a16d377970765d5fcfbe02e588c95d754a489
nasikkkkk <147999508+nasikkkkk@users.noreply.github.com>
2024-10-30 16:23:39 +0400

    Update LinkedListTabulatedFunctionSerialization.java

--- LR_3/src/main/java/ru/ssau/tk/nasikkkkk/Labs_OOP/io/LinkedListTabulatedFunctionSerialization.java
+++ LR_3/src/main/java/ru/ssau/tk/nasikkkkk/Labs_OOP/io/LinkedListTabulatedFunctionSerialization.java
@@ -1,7 +1,6 @@
 package ru.ssau.tk.nasikkkkk.Labs_OOP.io;
 
 import ru.ssau.tk.nasikkkkk.Labs_OOP.functions.LinkedListTabulatedFunction;
-
 import ru.ssau.tk.nasikkkkk.Labs_OOP.functions.TabulatedFunction;
 import ru.ssau.tk.nasikkkkk.Labs_OOP.operations.TabulatedDifferentialOperator;
 
@@ -10,10 +9,16 @@ import java.io.*;
 public class LinkedListTabulatedFunctionSerialization {
 
     public static void main(String[] args) {
-        String path = "output/serialized linked list functions.bin";
-        LinkedListTabulatedFunction originalFunction = new LinkedListTabulatedFunction(new ru.ssau.tk.nasikkkkk.Labs_OOP.io.AInDegreeXFunction(1, 2), 0, 10, 5);
+        String path = "output/serialized_linked_list_functions.bin";
+
+        // Создание исходной функции с использованием простого массива значений
+        double[] xValues = {0, 2.5, 5, 7.5, 10};
+        double[] yValues = {0, 6.25, 25, 56.25, 100}; // Пример значений для функции y = x^2
+
+        LinkedListTabulatedFunction originalFunction = new LinkedListTabulatedFunction(xValues, yValues);
         TabulatedFunction firstDerivative = new TabulatedDifferentialOperator().derive(originalFunction);
         TabulatedFunction secondDerivative = new TabulatedDifferentialOperator().derive(firstDerivative);
+
         try (FileOutputStream fileOutputStream = new FileOutputStream(path);
              BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream)) {
             FunctionsIO.serialize(bufferedOutputStream, originalFunction);
@@ -22,11 +27,13 @@ public class LinkedListTabulatedFunctionSerialization {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         try (FileInputStream fileInputStream = new FileInputStream(path);
              BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream)) {
             TabulatedFunction deserializedOriginalFunction = FunctionsIO.deserialize(bufferedInputStream);
             TabulatedFunction deserializedFirstDerivative = FunctionsIO.deserialize(bufferedInputStream);
             TabulatedFunction deserializedSecondDerivative = FunctionsIO.deserialize(bufferedInputStream);
+
             System.out.println(deserializedOriginalFunction.toString());
             System.out.println(deserializedFirstDerivative.toString());
             System.out.println(deserializedSecondDerivative.toString());
