2b3a0641fd16ba9b6f68ab4b320a640e9a47644c
nasikkkkk <147999508+nasikkkkk@users.noreply.github.com>
2024-10-31 14:30:48 +0400

    Delete LR_3/src/main/java/ru/ssau/tk/nasikkkkk/Labs_OOP/Main.java

--- LR_3/src/main/java/ru/ssau/tk/nasikkkkk/Labs_OOP/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.ssau.tk.nasikkkkk.Labs_OOP;
-
-import ru.ssau.tk.nasikkkkk.Labs_OOP.functions.*;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello");
-    }
-}
